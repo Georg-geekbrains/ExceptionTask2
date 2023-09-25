546d4ac61838dbfdb1557b675a617be4ddb0cebd
mac <georghome1@gmail.com>
2023-09-25 22:58:11 +0300

    Условие задачи

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+    /*
+    Если необходимо, исправьте данный код
+    (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+    */
 public class Main {
     public static void main(String[] args) {
         try {
